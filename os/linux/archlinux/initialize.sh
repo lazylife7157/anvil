2ff840456932004987054b93da1c99357bd1aa17
lazylife7157 <lazylife7157@gmail.com>
2017-07-30 18:35:00 +0900

    Refine initialize script

--- os/linux/archlinux/initialize.sh
+++ os/linux/archlinux/initialize.sh
@@ -1,44 +1,66 @@
-#!bin/bash
+#!/bin/bash
 
-# This script must run with sudo
+function log() {
+  echo "[ Anvil ] $1"
+}
 
-# Update the system clock
+if [ `whoami` != "root" ]
+then
+  log "This script must run with root privilege"
+  exit 1
+fi
+
+CURRENT_USER=`logname`
+log "Run by $CURRENT_USER"
+
+function cudo() {
+  su -c "$1" $CURRENT_USER
+}
+
+log "Update the system clock"
 timedatectl set-ntp true
 
-# Time zone
+log "Time zone"
 ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 hwclock --systohc
 
-# Install packages
+log "Install packages"
 pacman -Sy
-pacman -S --noconfirm \
+pacman -S --needed --noconfirm \
   net-tools \
-  cmake make \
+  gcc cmake make \
   rust \
   libglvnd
 
 if [ -n `lspci -k | grep VGA | grep NVIDIA` ]
 then
-  pacman -S --noconfirm libglvnd nvidia
+  log "Install NVIDIA driver"
+  pacman -S --needed --noconfirm libglvnd nvidia
 fi
 
 if [ -z `echo $PATH | grep '~/.cargo/bin'` ]
 then
-  echo 'export $PATH=$PATH:~/.cargo/bin' >> /etc/profile
+  log "Add cargo binary directory to PATH environment variable"
+  echo 'export PATH=$PATH:~/.cargo/bin' >> /etc/profile
+  cudo "source /etc/profile"
 fi
 
-# Install way-cooler
-pacman -S --noconfirm wayland wlc cargo cairo
-cargo install way-cooler
-cargo install way-cooler-bg
+log "Install way-cooler"
+pacman -S --needed --noconfirm wayland wlc cargo cairo
+cudo "cargo install way-cooler"
 
-# Install gdm
-pacman -S --noconfirm libglvnd gdm
+log "Install way-cooler-bg"
+cudo "cargo install way-cooler-bg"
 
-# Install alacritty
-pacman -S --noconfirm cmake freetype2 fontconfig pkg-config make xclip
-mkdir ~/git
-cd ~/git
-git clone https://github.com/jwilm/alacritty alacritty
-cd alacritty
-cargo build --release && cargo install
+# log "Install gdm"
+# pacman -S --needed --noconfirm libglvnd gdm
+
+log "Install alacritty"
+pacman -S --needed --noconfirm cmake freetype2 fontconfig pkg-config make xclip
+DIR_GIT=/home/$CURRENT_USER/git
+if [ ! -d $DIR_GIT ]
+then
+  mkdir $DIR_GIT
+fi
+cudo "git clone https://github.com/jwilm/alacritty ~/git/alacritty"
+cudo "cd ~/git/alacritty && cargo build --release && cargo install"
